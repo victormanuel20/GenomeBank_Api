bfa635a9497b0de992faaf00910b4e949a4a5c48
victormanuel20 <victormanuel20034@gmail.com>
2025-10-13 21:12:04 -0500

    Inicio de proyecto

---
+++ GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/GenomeBankApiApplication.java
@@ -0,0 +1,13 @@
+package com.developers.GenomeBank_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GenomeBankApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GenomeBankApiApplication.class, args);
+	}
+
+}
---
+++ GenomeBank_Api/src/test/java/com/developers/GenomeBank_Api/GenomeBankApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers.GenomeBank_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GenomeBankApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
