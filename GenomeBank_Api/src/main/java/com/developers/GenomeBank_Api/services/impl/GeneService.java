e0c3cae40eeb0e673f93c389319545e338b6e5b0
Satanasesmipator <luise.garciav@autonoma.edu.co>
2025-10-17 16:32:19 -0500

    Creando GeneController

--- GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/impl/GeneService.java
+++ GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/impl/GeneService.java
@@ -1,7 +1,11 @@
 package com.developers.GenomeBank_Api.services.impl;
 
+import com.developers.GenomeBank_Api.models.entities.Gene;
+import com.developers.GenomeBank_Api.repositories.GeneRepository;
 import com.developers.GenomeBank_Api.services.IGeneService;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 /**
  * Implementación del servicio de genes.
@@ -10,4 +14,22 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GeneService implements IGeneService {
+
+    private GeneRepository geneRepository;
+
+    /**
+     * Constructor que inyecta el repositorio de autores.
+     * @param geneRepository repositorio de autores
+     */
+    public GeneService(GeneRepository geneRepository) {
+        this.geneRepository = geneRepository;
+    }
+
+    @Override
+    public Optional<Gene> getGene(Long id) {return this.geneRepository.findById(id);
+
+    }
+
+
+
 }
