af25041a0e1e46a00b57cd848822d85255eacbec
victormanuel20 <victormanuel20034@gmail.com>
2025-10-18 03:48:53 -0500

    Estructura para crear genoma
    # Conflicts:
    #	GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/models/dto/ResultDTO.java
    #	GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/models/entities/Species.java
    #	GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/ISpeciesService.java
    #	GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/impl/SpeciesService.java
    
    # Conflicts:
    #	GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/impl/GenomeService.java

--- GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/impl/GenomeService.java
+++ GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/services/impl/GenomeService.java
@@ -1,13 +1,6 @@
 package com.developers.GenomeBank_Api.services.impl;
 
-import com.developers.GenomeBank_Api.models.dto.genome.CreateGenomeInDTO;
-import com.developers.GenomeBank_Api.models.dto.genome.CreateGenomeOutDTO;
-import com.developers.GenomeBank_Api.models.entities.Genome;
-import com.developers.GenomeBank_Api.models.entities.Species;
-import com.developers.GenomeBank_Api.repositories.GenomeRepository;
-import com.developers.GenomeBank_Api.repositories.SpeciesRepository;
 import com.developers.GenomeBank_Api.services.IGenomeService;
-import com.developers.GenomeBank_Api.services.ISpeciesService;
 import org.springframework.stereotype.Service;
 
 /**
@@ -17,49 +10,4 @@ import org.springframework.stereotype.Service;
  */
 @Service
 public class GenomeService implements IGenomeService {
-
-    private final GenomeRepository genomeRepository;
-    private final ISpeciesService speciesService;
-    private final SpeciesRepository speciesRepository;
-
-    public GenomeService(GenomeRepository genomeRepository,
-                         ISpeciesService speciesService,
-                         SpeciesRepository speciesRepository) {
-        this.genomeRepository = genomeRepository;
-        this.speciesService = speciesService;
-        this.speciesRepository = speciesRepository;
-    }
-
-    @Override
-    public CreateGenomeOutDTO createGenome(CreateGenomeInDTO createGenomeInDTO) {
-
-        CreateGenomeOutDTO outDTO = new CreateGenomeOutDTO();
-
-        // 1️⃣ Validate if the species exists
-        if (!this.speciesService.existsSpecies(createGenomeInDTO.getSpeciesId())) {
-            outDTO.setSuccess(false);
-            outDTO.setErrorMessage("The specified species does not exist.");
-            return outDTO;
-        }
-
-        // 2️⃣ Create the Genome entity
-        Genome genome = new Genome();
-        genome.setVersion(createGenomeInDTO.getVersion());
-
-        // Get species reference by ID (no need to load the entire entity)
-        Species species = speciesRepository.getReferenceById(createGenomeInDTO.getSpeciesId());
-        genome.setSpecies(species);
-
-        // 3️⃣ Save the Genome in the database
-        Genome savedGenome = this.genomeRepository.save(genome);
-
-        // 4️⃣ Prepare and return output DTO
-        outDTO.setSuccess(true);
-        outDTO.setId(savedGenome.getId());
-        outDTO.setVersion(savedGenome.getVersion());
-        outDTO.setSpeciesId(savedGenome.getSpecies().getId());
-
-        return outDTO;
-    }
-
 }
