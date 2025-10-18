f53e55d6f0f7b94636b9cc1899edbd4f1f4656c7
victormanuel20 <victormanuel20034@gmail.com>
2025-10-18 04:50:09 -0500

    Revert "se intenta hacer algo"
    This reverts commit 19802a123555ddc4cc58bed88a772469b26a4cea, reversing
    changes made to 232da2dde84c57cea7d7ddb5f79429a528d3d47a.

--- GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/controllers/SpeciesController.java
+++ GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/controllers/SpeciesController.java
@@ -1,15 +1,7 @@
 package com.developers.GenomeBank_Api.controllers;
 
-import com.developers.GenomeBank_Api.models.dto.speciesDtos.CreateSpeciesInDTO;
-import com.developers.GenomeBank_Api.models.dto.speciesDtos.CreateSpeciesOutDTO;
-import com.developers.GenomeBank_Api.models.dto.speciesDtos.GetSpeciesByIdOutDTO;
-import com.developers.GenomeBank_Api.models.dto.speciesDtos.GetAllSpeciesOutDTO;
-import com.developers.GenomeBank_Api.services.ISpeciesService;
-import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Controlador REST para la gestión de especies.
@@ -19,55 +11,4 @@ import java.util.List;
 @RestController
 @RequestMapping("/species")
 public class SpeciesController {
-    /**
-     * Se llama el servicio de especies
-     * para obtener el resultado de la lógica de negocio
-     */
-    private final ISpeciesService speciesService;
-
-    /**
-     * Se inyecta el servicio de especies
-     * para hacer uso de él
-     */
-    public SpeciesController(ISpeciesService speciesService) {
-        this.speciesService = speciesService;
-    }
-
-    /**
-     * Obtiene la lista de todos las especies.
-     * Solo usuarios con el rol USER y ADMIN pueden acceder.
-     * @return ResponseEntity con la lista de especies
-     */
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    @GetMapping("")
-    public ResponseEntity<List<GetAllSpeciesOutDTO>> getAllSpecies() {
-        List<GetAllSpeciesOutDTO> species = speciesService.getAllSpecies();
-        return ResponseEntity.ok(species);
-    }
-
-    /**
-     * Obtiene la especie por ID.
-     * Solo usuarios con el rol USER y ADMIN pueden acceder.
-     * @return ResponseEntity con la especie
-     */
-    @PreAuthorize("hasAnyRole('USER','ADMIN')")
-    @GetMapping("/{id}")
-    public ResponseEntity<GetSpeciesByIdOutDTO> getSpeciesById(@PathVariable Long id) {
-        GetSpeciesByIdOutDTO species = speciesService.getSpeciesById(id);
-        return ResponseEntity.ok(species);
-    }
-
-    /**
-     * Crea la especie con DTO
-     * @param createSpeciesInDTO
-     * @return ResponseEntity
-     */
-    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("")
-    public ResponseEntity<CreateSpeciesOutDTO> createSpecies(@RequestBody CreateSpeciesInDTO
-                                                                      createSpeciesInDTO) {
-
-        return ResponseEntity.ok(this.speciesService.createSpecies(createSpeciesInDTO));
-    }
-
 }
