1c28d19bc740fc43fdf0c0c451f28112c40ba85f
Satanasesmipator <luise.garciav@autonoma.edu.co>
2025-10-19 18:47:05 -0500

    Agregando relacion M a M

--- GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/models/dto/geneDtos/CreateGeneOutDTO.java
+++ GenomeBank_Api/src/main/java/com/developers/GenomeBank_Api/models/dto/geneDtos/CreateGeneOutDTO.java
@@ -1,4 +1,6 @@
-package com.developers.GenomeBank_Api.models.dto;
+package com.developers.GenomeBank_Api.models.dto.geneDtos;
+
+import com.developers.GenomeBank_Api.models.dto.ResultDTO;
 
 public class CreateGeneOutDTO extends ResultDTO {
     private Long geneId;
